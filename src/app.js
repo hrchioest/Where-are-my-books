01fbb26c60040a50c4b1c03c8f8431fd1feb8b85
Rocío Esteban <53129859+hrchioest@users.noreply.github.com>
2020-12-22 14:48:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const mysql = require("mysql");
-const util = require("util");
+const booksRouter = require("./routes/books");
+const usersRouter = require("./routes/users");
+const categoriesRouter = require("./routes/categories");
 
 //Setting
 const app = express();
@@ -9,28 +10,10 @@ const port = process.env.PORT || 3000;
 //Middleswares
 app.use(express.json());
 
-// Conexion con mysql
-const conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "mybooks"
-});
-
-conexion.connect((error) => {
-  if (error) {
-    throw error;
-  }
-
-  console.log("Conexion con la base de datos mysql establecida");
-});
-
-const qy = util.promisify(conexion.query).bind(conexion); // permite el uso de asyn-await en la conexion mysql
-
-//Routes
-app.use(require("./routes/books"));
-app.use(require("./routes/users"));
-app.use(require("./routes/categories"));
+// rutas
+app.use("/books", booksRouter);
+app.use("/users", usersRouter);
+app.use("/categories", categoriesRouter);
 
 // Starting the server
 app.listen(port, () => {
