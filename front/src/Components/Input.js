403164cccf77a129d415c0aa3fe9e4e7199bb96c
joaquinzuazo <jzuazociolfi@gmail.com>
2021-02-25 09:11:31 -0300

    parcial newlibro

--- front/src/Components/Input.js
+++ front/src/Components/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Input extends React.Component {
     render(){
-        const input=<input type="text" name={this.props.name} placeholder={this.props.name}></input>
+        const input=<input type="text" name={this.props.name} placeholder={this.props.name} onChange={this.props.accion}></input>
         return input
     }
 }
--- front/src/Components/NewLibro.js
+++ front/src/Components/NewLibro.js
@@ -3,14 +3,38 @@ import axios from 'axios';
 import Input from './Input'
 
 class NewLibro extends React.Component {
+  state = {
+    nombre: '',
+    descripcion: '',
+    categoria_id: '',
+    persona_id: '',
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleClick = () => {
+    const values=JSON.stringify(this.state)
+    axios.post(`http://localhost:3000/libro`, {
+      values
+    }).then((data)=>{
+      console.log(data);
+    }).catch((e)=>{
+    console.log(e)
+  })
+  }
+
+
   render(){
     return (
       <div className='newLibro-form'>
-        <Input name='Nombre' />
-        <Input name='Descripcion' />
-        <Input name='ID Categoria' />
-        <Input name='ID Persona' />
-        <button type='submit'>Guardar</button>
+        <Input name='nombre' accion={this.handleChange} />
+        <Input name='descripcion' accion={this.handleChange} />
+        <Input name='categoria_id' accion={this.handleChange}/>
+        <Input name='persona_id' accion={this.handleChange}/>
+        <button onClick={()=>this.handleClick()}>Guardar</button>
       </div>
     )
   }
