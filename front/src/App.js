160196a8b70627ca5603082082770b31a28bbfb6
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-02-28 18:31:26 -0300

    se modifica nombres de archivos

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import PersonForm from "./components/PersonForm/PersonForm";
-import TableBooks from "./components/TableBooks/TableBooks";
 import TablePersons from "./components/TablePersons/TablePersons";
-
 import { DataProvider } from "./context/DataContext";
 
 function App() {
